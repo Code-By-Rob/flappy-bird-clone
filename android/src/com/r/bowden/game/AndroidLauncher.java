2e60b8f15b8ba864dec1859b7853180fd96a036e
Saphfy <robbiebowden99@gmail.com>
2020-12-11 19:32:58 +0000

    Initial Commit.

---
+++ android/src/com/r/bowden/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.r.bowden.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.r.bowden.game.FalppyBird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FalppyBird(), config);
+	}
+}
